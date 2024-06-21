3536c4ddb32f8f8716dfba7711d3883718cb22be
David O'Sullivan <davidos@vmware.com>
2024-06-21 17:09:06 +0100

    supports more specific plan combinations for PHP + web servers

--- dt/detect.go
+++ dt/detect.go
@@ -42,6 +42,26 @@ func (d Detect) Detect(context libcnb.DetectContext) (libcnb.DetectResult, error
 		Plans: []libcnb.BuildPlan{
 			{
 				Provides: []libcnb.BuildPlanProvide{
+					{Name: "dynatrace-php"},
+				},
+				Requires: []libcnb.BuildPlanRequire{
+					{Name: "dynatrace-php"},
+					{Name: "php"},
+					{Name: "httpd"},
+				},
+			},
+			{
+				Provides: []libcnb.BuildPlanProvide{
+					{Name: "dynatrace-php"},
+				},
+				Requires: []libcnb.BuildPlanRequire{
+					{Name: "dynatrace-php"},
+					{Name: "php"},
+					{Name: "nginx"},
+				},
+			},
+			{
+				Provides: []libcnb.BuildPlanProvide{
 					{Name: "dynatrace-apache"},
 				},
 				Requires: []libcnb.BuildPlanRequire{
